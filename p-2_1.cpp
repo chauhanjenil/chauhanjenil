bbd56ffd73f7307fa53c0c2b858670b79a788ca6
chauhanjenil <160855550+chauhanjenil@users.noreply.github.com>
2024-02-22 17:27:21 +0530

    Add files via upload

---
+++ p-2_1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"enter two numbers : \n";
+	cin>>a>>b;
+	c=a+b;
+	cout<<"\naddition is         : "<<c;
+	c=a-b;
+	cout<<"\nsubstraction is     : "<<c;
+	c=a*b;
+	cout<<"\nmultiplication is   : "<<c;
+	c=a/b;
+	cout<<"\ndivision is         : "<<c;
+	
+	return 0;
+}
